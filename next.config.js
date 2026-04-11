dbccbf61bc318fda4ea21b4f4c5ee10eb3ae12c2
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2026-04-11 21:53:59 +0300

    build: updated next config

--- next.config.js
+++ next.config.js
@@ -12,6 +12,8 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: "export",
+
 	// Optimize production builds
 	reactStrictMode: true,
 
